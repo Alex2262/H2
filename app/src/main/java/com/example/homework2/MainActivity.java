7cc8f924e5f3f0018ba8c97e07f1f25de6aaaa58
Alexander Tian <alex2262alexander@gmail.com>
2022-07-19 19:31:35 -0400

    Bugs fixed

--- app/src/main/java/com/example/homework2/MainActivity.java
+++ app/src/main/java/com/example/homework2/MainActivity.java
@@ -14,8 +14,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView title;
     private Button clearButton;
     private Button[] buttonBoxes = new Button[9];
-    private int[] board = new int[9];
-    private int turn = 0;
+    private int[] board = new int[9];  // 0 == Empty, 1 == O, 2 == X
+    private int turn = 0; // 0 for O, 1 for X
 
     private void updateBoard() {
         for (int i = 0; i < 9; ++i) {
@@ -37,21 +37,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private Boolean checkForWin() {
-        if (board[0] == board[1] && board[1] == board[2])
+        if (board[0] != 0 && board[0] == board[1] && board[1] == board[2])
             return true;
-        if (board[3] == board[4] && board[4] == board[5])
+        if (board[3] != 0 && board[3] == board[4] && board[4] == board[5])
             return true;
-        if (board[6] == board[7] && board[7] == board[8])
+        if (board[6] != 0 && board[6] == board[7] && board[7] == board[8])
             return true;
-        if (board[0] == board[3] && board[3] == board[6])
+        if (board[0] != 0 && board[0] == board[3] && board[3] == board[6])
             return true;
-        if (board[1] == board[4] && board[4] == board[7])
+        if (board[1] != 0 && board[1] == board[4] && board[4] == board[7])
             return true;
-        if (board[2] == board[5] && board[5] == board[8])
+        if (board[2] != 0 && board[2] == board[5] && board[5] == board[8])
             return true;
-        if (board[0] == board[4] && board[4] == board[8])
+        if (board[0] != 0 && board[0] == board[4] && board[4] == board[8])
             return true;
-        if (board[2] == board[4] && board[4] == board[6])
+        if (board[2] != 0 && board[2] == board[4] && board[4] == board[6])
             return true;
         return false;
     }
@@ -79,18 +79,22 @@ public class MainActivity extends AppCompatActivity {
             buttonBoxes[i].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (board[finalI] == 0) {
-                        // Toast.makeText(this, "Tile has been filled", Toast.LENGTH_LONG).show();
-                        System.out.println("Tile has been filled");
+                    if (board[finalI] != 0) {
+                        Toast.makeText(getApplicationContext(), "Tile has been filled",
+                                Toast.LENGTH_LONG).show();
+                        // System.out.println("Tile has been filled");
                     } else {
                         board[finalI] = turn + 1;
                         turn ^= 1;
-                    }
-                    updateBoard();
 
-                    if (checkForWin()) {
-                        System.out.println("Won");
+                        updateBoard();
+
+                        if (checkForWin()) {
+                            Toast.makeText(MainActivity.this, "Game Won",
+                                    Toast.LENGTH_LONG).show();
+                        }
                     }
+
                 }
             });
         }
